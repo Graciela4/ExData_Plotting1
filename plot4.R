3532106ff3d8f0a8bad3f9c32d1d2402a58ca0c9
siny <sinytrie@gmail.com>
2020-08-14 18:48:03 +0200

    update plot 4 to increase legend size

--- plot4.R
+++ plot4.R
@@ -20,7 +20,7 @@ with(data, plot(datetime, Voltage, type = "l" ))
 with(data, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
 points(data$datetime, data$Sub_metering_2, type = "l", col = "red")
 points(data$datetime, data$Sub_metering_3, type = "l", col = "blue")
-legend("topright", legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, cex = 0.5 )
+legend("topright", legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, cex = 0.9 )
 ## Fourth plot
 with(data, plot(datetime, Global_reactive_power, type = "l"))
 dev.off()
