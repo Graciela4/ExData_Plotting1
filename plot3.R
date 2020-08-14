3096d65ddcbe3acc5a131dca965558475fb759c3
siny <sinytrie@gmail.com>
2020-08-14 18:43:43 +0200

    R script for plot 3

---
+++ plot3.R
@@ -0,0 +1,18 @@
+library(lubridate)
+
+## Reading in file, skipping rows 
+header <- read.table("household_power_consumption.txt", nrows = 1, header = FALSE, sep =';', stringsAsFactors = FALSE)
+data <- read.table(file="household_power_consumption.txt", header= F, col.names = header , sep=";", dec=".", na.strings="?", skip = grep("^[1,2]/2/2007", readLines("household_power_consumption.txt"))-1, nrows=2880 )
+
+# Transform Date variable 
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$datetime <- as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
+
+## Plot 3
+png(filename = "plot3.png")
+with(data, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+points(data$datetime, data$Sub_metering_2, type = "l", col = "red")
+points(data$datetime, data$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1 )
+dev.off()
